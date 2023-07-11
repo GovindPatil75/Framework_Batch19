78a7fd3a5446fe8d23d8124dd06633907077bcf9
lalitdudhegithub <lalitdudhe09@gmail.com>
2023-07-11 10:08:18 +0530

    TC_004 Added

---
+++ src/test/java/com/LoginTest/VerifyTC_004.java
@@ -0,0 +1,12 @@
+package com.LoginTest;
+
+import org.testng.annotations.Test;
+
+public class VerifyTC_004 {
+
+	@Test
+	public void VerifyRegistration() {
+		
+		System.out.println("push to local repo");
+	}
+}
