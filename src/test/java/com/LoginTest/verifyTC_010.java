e723ddd3f4d938e6d6934619b1ff8c29e3eb4a54
vaishnavibangad <vaishnavibangad27@gmail.com>
2023-07-12 18:40:14 +0530

    test case010 added

---
+++ src/test/java/com/LoginTest/verifyTC_010.java
@@ -0,0 +1,12 @@
+package com.LoginTest;
+
+import org.testng.annotations.Test;
+
+public class verifyTC_010 {
+	@Test
+	public void VerifyRegistration() {
+		
+		System.out.println("push to local repository");
+	}
+
+}
